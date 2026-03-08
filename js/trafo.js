934d9bbb2919bffb1089116742f86810c74e9ec2
Thiago Dos Santos Araujo <thiagoshield199403@gmail.com>
2026-03-07 23:22:39 -0300

    Update trafo.js

--- js/trafo.js
+++ js/trafo.js
@@ -52,28 +52,28 @@ function gerarTrafoSGD(){
   const texto =
 
 `Motivo:${v("tf_motivo")}
-#Ocorrência:${v("tf_ocorrencia")}
-#Regional:${v("tf_regional")}
-#Operador:${v("tf_operador")}
-#Endereço:${v("tf_endereco")}
-#Coordenadas:${v("tf_coord")}
-#Trafo:${v("tf_trafo")}
-#Potência:${v("tf_potencia")}
-#Classe:${v("tf_classe")}
-#Tensão Secundária:${v("tf_tensao_sec")}
-#Tipo Cabo:${v("tf_tipo_cabo")}
-#Carregamento Trafo:${v("tf_carregamento")}
-#Clientes Desenergizados:${v("tf_clientes_des")}
-#Total Clientes:${v("tf_total_clientes")}
-#ParaRaio MT:${v("tf_pr_mt")}
-#ParaRaio BT:${v("tf_pr_bt")}
-#Aterramento:${v("tf_aterramento")}
-#Corrosão Atmosférica:${v("tf_corrosao")}
-#Circuito Transferido:${v("tf_transferido")}
-#Possui GLV:${v("tf_glv")}
-#GLV Extraido:${v("tf_glv_extraido")}
-#Materiais Necessários:${v("tf_materiais")}
-#Obs:${v("tf_obs")}`;
+-Operador:${v("tf_operador")}
+-Ocorrência:${v("tf_ocorrencia")}
+-Regional:${v("tf_regional")}
+-Endereço:${v("tf_endereco")}
+-Coordenadas:${v("tf_coord")}
+-Trafo:${v("tf_trafo")}
+-Potência:${v("tf_potencia")}
+-Classe:${v("tf_classe")}
+-Tensão Secundária:${v("tf_tensao_sec")}
+-Tipo Cabo:${v("tf_tipo_cabo")}
+-Carregamento Trafo:${v("tf_carregamento")}
+-Clientes Desenergizados:${v("tf_clientes_des")}
+-Total Clientes:${v("tf_total_clientes")}
+-ParaRaio MT:${v("tf_pr_mt")}
+-ParaRaio BT:${v("tf_pr_bt")}
+-Aterramento:${v("tf_aterramento")}
+-Corrosão Atmosférica:${v("tf_corrosao")}
+-Circuito Transferido:${v("tf_transferido")}
+-Possui GLV:${v("tf_glv")}
+-GLV Extraido:${v("tf_glv_extraido")}
+-Materiais Necessários:${v("tf_materiais")}
+-Obs:${v("tf_obs")}`;
 
   tf_resultado.value = texto;
   navigator.clipboard.writeText(texto);
@@ -88,3 +88,4 @@ function limparTrafo(){
     .forEach(e => e.selectedIndex = 0);
   tf_resultado.value = "";
 }
+
